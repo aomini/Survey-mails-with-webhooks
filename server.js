8c629acefc9185bbefa9bfe512706c6fded301fd
rakeshShubhu <rak3sh.shrestha@gmail.com>
2019-08-04 00:40:15 +0545

    fixes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 // Route handler
-// app.get("/", (req, res) => {
-//   res.send("<h1>ress</h1>");
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>ress</h1>");
+});
 
 //Dynamically figure which port to listen
 const PORT = process.env.PORT || 9000;
