f4f823606e2d764a3baa780790d7282cf47cc700
rakeshShubhu <rak3sh.shrestha@gmail.com>
2019-08-04 00:37:50 +0545

    package

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 // Route handler
-app.get("/", (req, res) => {
-  res.send("<h1>ress</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>ress</h1>");
+// });
 
 //Dynamically figure which port to listen
-// const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000;
 
-app.listen(9000);
+app.listen(PORT);
