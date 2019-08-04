cab1e0e810f8681f8124dd998f4c3b3ffc0a256e
rakeshShubhu <rak3sh.shrestha@gmail.com>
2019-08-04 20:32:48 +0545

    update with proxy

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: googleClientID,
       clientSecret: googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
